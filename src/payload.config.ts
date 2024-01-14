6c263d8025446c235e167ba8051e8b7ef1918613
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-01-14 22:29:38 +0300

    add mongooseAdapter webpackBundler slateEditor for payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,7 +1,7 @@
 import { buildConfig } from "payload/config";
-// import { webpackBundler } from "@payloadcms/bundler-webpack";
-// import { mongooseAdapter } from "@payloadcms/db-mongodb";
-// import { slateEditor } from "@payloadcms/richtext-slate";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 // import { Users } from "./collections/Users";
 import dotenv from "dotenv";
@@ -16,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users, Products, Media, ProductFiles, Orders],
+  collections: [],
   routes: {
     admin: "/sell",
   },
